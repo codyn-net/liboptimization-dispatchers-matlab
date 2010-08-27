c0ebe086122b1fe97beb2920cfb9257efa14bb72
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2010-08-27 14:43:08 +0200

    Added load_workspace

---
+++ load_workspace.m
@@ -0,0 +1,16 @@
+function load_sim_workspace(workspace)
+	if ~exist(workspace)
+		return
+	end
+
+	disp('Loading the simulation workspace...');
+	data = load(workspace);
+
+	names = fieldnames(data);
+
+	for name = 1:length(names)
+		n = names{name};
+
+		evalin('base', ['global ', char(n)]);
+		assignin('base', n, data.(n));
+	end
