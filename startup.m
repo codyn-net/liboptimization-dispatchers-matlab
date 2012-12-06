00eed80d8eb3d6c6227659666a05c10b5fa6e707
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2012-12-06 12:26:16 +0100

    Search for correct protobuf jar file

--- startup.m
+++ startup.m
@@ -1,6 +1,16 @@
+% Main entry point for the matlab dispatcher
+
 javaaddpath('.');
 javaaddpath('protobuf/build');
-javaaddpath('/usr/share/java/protobuf-2.2.0.jar');
+
+jars = {'/usr/share/java/protobuf.jar', '/usr/share/java/protobuf-java.jar'};
+
+for i = 1:length(jars)
+    if exist(jars{i}, 'file')
+        javaaddpath(jars{i});
+        break
+    end
+end
 
 if strcmp(getenv('OPTIMIZATION_EXTERNAL'), '') == 0
     optimization(str2num(getenv('OPTIMIZATION_EXTERNAL_PERSISTENT')));
